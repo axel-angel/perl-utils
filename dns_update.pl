59f1997221667617761184d06685a38c679d0362
Axel Angel <axel+git@vneko.ch>
2014-11-15 16:02:35 +0100

    dns_update: Add doc and option --algorithm

--- dns_update.pl
+++ dns_update.pl
@@ -2,6 +2,34 @@
 use strict;
 use warnings;
 
+=pod
+Generate a key (keep it safe):
+> dnssec-keygen -a HMAC-SHA512 -b 512 -n HOST mykeyname
+
+Take the value of Key in the generated file *.private (use as mysecret).
+Add into your bind9 named.conf.local (replace mykeyname and mysecret):
+  key "mykeyname" {
+      algorithm "HMAC-SHA512";
+      secret "mysecret";
+  };
+
+Add the zone too (replace mykeyname and myname.ddns.example.com):
+  zone "ddns.example.com" IN {
+      type master;
+      file "example.com.ddns.zone";
+      update-policy {
+          grant mykeyname name myname.ddns.example.com;
+      };
+  };
+
+Then reload bind9 config.
+
+Usage example:
+> perl dns_update.pl --verbose --server ns1.example.com --zone ddns.example.com --name myhost --address 127.0.0.1 --keyfile mykey --keyname myname
+
+Check your bind9 logs to see if it went well.
+=cut
+
 use Getopt::ArgParse;
 use Net::DNS::Update;
 use Net::DNS;
@@ -18,6 +46,7 @@ $ap->add_arg('--name', '-n', type => 'Scalar', required => 1);
 $ap->add_arg('--address', '-a', type => 'Scalar', required => 1);
 $ap->add_arg('--keyfile', '-k', type => 'Scalar', required => 1);
 $ap->add_arg('--keyname', '-i', type => 'Scalar', required => 1);
+$ap->add_arg('--keyalgo', type => 'Scalar', default => 'HMAC-SHA512');
 
 my $a = $ap->parse_args();
 
@@ -53,7 +82,7 @@ $update->push(update => rr_add($record));
 my $tsig = Net::DNS::RR->new(
     name => $a->keyname,
     type => 'TSIG',
-    algorithm => 'HMAC-SHA512',
+    algorithm => $a->keyalgo,
     key => $key,
 );
 $update->sign_tsig($tsig);
